55d515ff66b63182856f4612bee9bf0f5be29499
MeilakhSergey <meylakhsergey@gmail.com>
2018-12-15 16:02:11 +0300

    Passed all tests++

--- src/Snail.java
+++ src/Snail.java
@@ -25,74 +25,37 @@ public class Snail {
         boolean[][] isPassed = new boolean[n][m];
         int[] result = new int[n * m];
 
-        Integer i = 0;
-        Integer j = 0, k = 0;
+        int i = 0, j = 0, k = 0;
         result[k] = array[i][j];
         isPassed[i][j] = true;
 
         while (k < n*m - 1) {
-            movingRight(i, j, array, k, isPassed, result);
-//            while (j+1 <= m-1 && !isPassed[i][j+1]) {
-//                j++;
-//                k++;
-//                isPassed[i][j] = true;
-//                result[k] = array[i][j];
-//            }
-            movingDown(i, j, array, k, isPassed, result);
-//            while (i+1 <= n-1 && !isPassed[i+1][j]) {
-//                i++;
-//                k++;
-//                isPassed[i][j] = true;
-//                result[k] = array[i][j];
-//            }
-            movingLeft(i, j, array, k, isPassed, result);
-//            while (j-1 >= 0 && !isPassed[i][j-1]) {
-//                j--;
-//                k++;
-//                isPassed[i][j] = true;
-//                result[k] = array[i][j];
-//            }
-            movingUp(i, j, array, k, isPassed, result);
-//            while (i-1 >= 0 && !isPassed[i-1][j]) {
-//                i--;
-//                k++;
-//                isPassed[i][j] = true;
-//                result[k] = array[i][j];
-//            }
+            while (j+1 <= m-1 && !isPassed[i][j+1]) {
+                j++;
+                k++;
+                isPassed[i][j] = true;
+                result[k] = array[i][j];
+            }
+            while (i+1 <= n-1 && !isPassed[i+1][j]) {
+                i++;
+                k++;
+                isPassed[i][j] = true;
+                result[k] = array[i][j];
+            }
+            while (j-1 >= 0 && !isPassed[i][j-1]) {
+                j--;
+                k++;
+                isPassed[i][j] = true;
+                result[k] = array[i][j];
+            }
+            while (i-1 >= 0 && !isPassed[i-1][j]) {
+                i--;
+                k++;
+                isPassed[i][j] = true;
+                result[k] = array[i][j];
+            }
         }
-
         return result;
     }
-    private static void movingRight(Integer i, Integer j, int[][] array, Integer k, boolean[][] isPassed, int[] result) {
-        while (j+1 <= array[0].length-1 && !isPassed[i][j+1]) {
-            j++;
-            k++;
-            isPassed[i][j] = true;
-            result[k] = array[i][j];
-        }
-    }
-    private static void movingDown(Integer i, Integer j, int[][] array, Integer k, boolean[][] isPassed, int[] result) {
-        while (i+1 <= array.length-1 && !isPassed[i+1][j]) {
-            i++;
-            k++;
-            isPassed[i][j] = true;
-            result[k] = array[i][j];
-        }
-    }
-    private static void movingLeft(Integer i, Integer j, int[][] array, Integer k, boolean[][] isPassed, int[] result) {
-        while (j-1 >= 0 && !isPassed[i][j-1]) {
-            j--;
-            k++;
-            isPassed[i][j] = true;
-            result[k] = array[i][j];
-        }
-    }
-    private static void movingUp(Integer i, Integer j, int[][] array, Integer k, boolean[][] isPassed, int[] result) {
-        while (i-1 >= 0 && !isPassed[i-1][j]) {
-            i--;
-            k++;
-            isPassed[i][j] = true;
-            result[k] = array[i][j];
-        }
-    }
+
 }
